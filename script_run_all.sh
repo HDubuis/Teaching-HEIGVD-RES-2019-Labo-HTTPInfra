fe015dc48cc09755a938cd2704a7964cd913ad4f
isaia.spinelli <isaia.spinelli@heig-vd.ch>
2019-06-01 16:42:08 +0200

    Add script run all + mov file express-images

---
+++ script_run_all.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+
+# run static apache
+# http://demo.res.ch:8080/
+docker run -d res_apache_php
+# run dynamic express
+# http://demo.res.ch:8080/api/animals/
+docker run -d res_express
+# run proxy reverse
+docker run -d -p 8080:80 res/apache_rp
