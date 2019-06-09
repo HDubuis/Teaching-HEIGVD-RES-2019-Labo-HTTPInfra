c5522e81d7a66b0bf7df480a8e1f7b153b2d7f5c
Thorkal <tommy.gerardi@hotmail.com>
2019-06-09 18:05:01 +0200

    Ajout de gestion UI via portainer

--- script_run_all.sh
+++ script_run_all.sh
@@ -39,5 +39,9 @@ echo $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" apache_static):80
 echo $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" express_dynamic):3000
 # lance le proxy en dynamic a changer les IPs avec ce qui est afficher par le script
 docker run -d -e STATIC_APP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" apache_static):80 -e DYNAMIC_APP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" express_dynamic):3000 --name apache_rp -p 8080:80 res/apache_rp
-# Permet de laisser le temps de voir les IPs
+
+#enable the ui
+bash ./enable-ui-management.sh
+
+# Permet de laisser le temps de voir ce qui s'est passe
 read -p "Press [Enter] key to start backup..."
