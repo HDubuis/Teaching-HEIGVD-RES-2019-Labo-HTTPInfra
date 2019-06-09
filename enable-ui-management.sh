9c88256af600a329fd8e5df23f1e7e7457f2a30e
Thorkal <tommy.gerardi@hotmail.com>
2019-06-09 19:17:31 +0200

    Forgot to add the shell script to last commit

--- enable-ui-management.sh
+++ enable-ui-management.sh
@@ -1,2 +1,3 @@
 docker volume create portainer_data
-docker run -d -p 9000:9000 -v /var/run/docker.sock:/var/run/docker.sock -v portainer_data:/data portainer/portainer
+docker run -d -p 9000:9000 --name ui -v /var/run/docker.sock -v portainer_data:/data portainer/portainer
+read -p "Press [Enter] key to start backup..."
