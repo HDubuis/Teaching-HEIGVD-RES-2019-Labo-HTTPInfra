0cd83962a245035f4ae24c393934845b6d1e3f8f
Thorkal <tommy.gerardi@hotmail.com>
2019-06-10 17:04:48 +0200

    Traefik ajoute pour gerer dynamic cluster et sticky sessions

--- script_run_all.sh
+++ script_run_all.sh
@@ -4,17 +4,23 @@
 docker kill apache_static
 docker kill express_dynamic
 docker kill apache_rp
+docker kill traefik
 #docker kill ui
 # 2 kill stopped
 # docker rm `docker ps -qa`
 docker rm $(docker ps -a -q)
 
 # 3 rebuild
-bash ./images-docker/apache-php-image/script_build.sh
-bash ./images-docker/apache-reverse-proxy/script_build.sh
-bash ./images-docker/express-image/script_build.sh
-bash ./images-docker/traefik/script_build.sh
+bash ./images-docker/apache-php-image/script_build.sh/
+bash ./images-docker/apache-reverse-proxy/script_build.sh/
+bash ./images-docker/express-image/script_build.sh/
+bash ./images-docker/traefik/script_build.sh/
 
+#on run traefik
+echo "Launch traefik whith given command"
+read -p "docker run -d --name traefik -p 8080:80 -p 9090:9090 -v /var/run/docker.sock:/var/run/docker.sock res_traefik"
+#docker run -d --name traefik -p 8080:80 -p 9090:9090 -v /var/run/docker.sock:/var/run/docker.sock res_traefik
+echo "Traefik launched"
 # run static apache
 # http://demo.res.ch:8080/
 docker run -d --name apache_static res_apache_php
@@ -40,10 +46,10 @@ docker inspect express_dynamic | grep "IPAddress"
 echo $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" apache_static):80
 echo $(docker inspect -f "{{ .NetworkSettings.IPAddress }}" express_dynamic):3000
 # lance le proxy en dynamic a changer les IPs avec ce qui est afficher par le script
-docker run -d -e STATIC_APP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" apache_static):80 -e DYNAMIC_APP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" express_dynamic):3000 --name apache_rp -p 8080:80 res/apache_rp
+#docker run -d -e STATIC_APP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" apache_static):80 -e DYNAMIC_APP=$(docker inspect -f "{{ .NetworkSettings.IPAddress }}" express_dynamic):3000 --name apache_rp -p 8080:80 res/apache_rp
 
 #enable the ui
 #bash ./enable-ui-management.sh
 
 # Permet de laisser le temps de voir ce qui s'est passe
-read -p "Press [Enter] key to start backup..."
+read -p "Press [Enter] key to end"
